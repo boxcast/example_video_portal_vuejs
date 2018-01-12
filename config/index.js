cc8b49c1b2c7243952b5f8230dd5bbfd2bc30c84
Mike Griffith <bikegriffith@gmail.com>
2018-01-12 16:27:42 -0500

    deploy scripts

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/apps/example_video_portal_vuejs/',
 
     /**
      * Source Maps
