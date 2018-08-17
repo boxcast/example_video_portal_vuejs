42881de27358d5caeda9887b11243efd7380ae8c
Mike Griffith <bikegriffith@gmail.com>
2018-08-17 14:51:51 -0400

    fix deployment artifact; include bootstrap v4 separately to avoid css precedence issues

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import BroadcastCard from '@/components/BroadcastCard'
 import BroadcastRow from '@/components/BroadcastRow'
